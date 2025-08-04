21b3d96b699a138853f94073d1be11c579a646e9
laurentlaboise <110203851+laurentlaboise@users.noreply.github.com>
2025-08-04 23:50:33 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
 const nextConfig = {
   // Your Next.js configuration options can go here.
   // For now, we can leave it empty.
+  // This file needs to use the modern 'export default' syntax.
 };
 
 export default nextConfig;
